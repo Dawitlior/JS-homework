e9b69b832c1070af02621e09089f65b9c6776035
Dawitlior <dawitlior777@gmail.com>
2022-05-15 09:40:32 +0300

    js

---
+++ main.js
@@ -0,0 +1,223 @@
+// הדפסת הללו וורלד 5 פעמים לקונסול
+
+// function hello(){
+// console.log("hello world")
+// }
+// var i =0
+// while(i<5){
+//     (hello())
+//     i++
+// }
+
+
+//תכנית שמקבלת שני מספרים מחזירה את המקסימום עם return
+
+// function max(num1,num2){
+
+//     var num1 = +prompt("print a number")
+//     var num2 = +prompt("print a number")
+    
+//     if (num1>num2){
+//         return console.log(num1)
+        
+//     }
+//     else{
+//         return console.log(num2)
+//     }
+
+// }
+//     max()
+
+
+
+//תכנית שמקבלת שני מספרים מחזירה את הסכום שלהם עם return
+// function sum(num1,num2){ 
+// var num1 = +prompt("print a number")
+// var num2 = +prompt("print a number")
+// if (num1>=num2){
+//     return num1+num2
+// }
+// else{
+//     return num1+num2
+// }
+// }
+// console.log (sum())
+
+
+
+
+// function sum (sum){
+//     var num = +prompt("")
+// for(i=num-1;num>0;i--){
+
+
+// }
+
+
+// }
+
+
+
+// function israel(){
+//     console.log("israel")
+// }
+// israel()
+
+// function liorDawit(){
+    
+//     console.log("lior dawit")
+// }
+// liorDawit()
+
+
+
+// function number(){
+//     console.log(7)
+// }
+// number()
+
+
+
+// function numbers(){
+// for(i=0;i<5;i++){
+//     console.log(1)
+// }
+// }
+// numbers()
+
+// function numbers(){
+// var num1 = 1
+// var num2 = 2
+// var num3 = 3
+// var num4 = 4
+// var sum =num1+num2+num3+num4
+// var avg = sum/4
+
+// console.log(sum,avg)
+// }
+// numbers()
+
+
+
+// function area(){
+
+//     var hight = 8
+//     var wight = 4
+//     console.log(hight*wight)
+// }
+// area()
+
+
+
+// function vars (){
+//     var num1 = 4
+//     var num2 = 8
+//     console.log(num1,num2)
+//     var helpme = num2
+//     var num2 = num1
+//     console.log(helpme,num1)
+// }
+// vars()
+
+
+
+// function answare(){
+//     var ans = prompt("did U sign up already?")
+//     console.log(ans)
+// }
+// answare()
+
+
+
+
+// function ans(){
+//     var user = prompt("Did you over 18?")
+//     console.log(user)
+// }
+// ans()
+
+
+// function whatYourAge(){
+//     var age = +prompt("How older you?")
+//     if(age<18){
+//         document.write("Hello Motherfucker")
+//     }
+//     else if(age>18){
+//         document.write("goodbay sucker")
+//     }
+// }
+// whatYourAge()
+
+
+
+// function nums(){
+//     var num1 = +prompt("Type a number")
+//     var num2 = +prompt("Type a number")
+//     console.log(num1,num2)
+// }
+// nums()
+
+
+// function vars(){
+//     var a1 = ("a")
+//     var a2 = (1)
+//     console.log(a1,a2)
+// }
+// vars()
+
+// function bool(){
+//     var a = 2
+//     var b = 4
+//     console.log(a<b)
+// }
+// bool()
+
+// function whoIsBigger(){
+//     var a = 5
+//     var b = 7
+//     console.log(b>a,a<b)
+//     console.log(a>b,b<a)
+//     console.log(a<b,a==b)
+//     console.log(a>b,b>a)
+// }
+// whoIsBigger()
+
+
+// function sum(){
+//     var a = +prompt("Type number")
+//     var b = +prompt("Type number")
+//     var c = +prompt("Type number")
+//     var mul = a*b*c
+//     console.log(mul)
+// }
+// sum()
+
+// function modolo(){
+//     var first = +prompt("Type number")
+//     var second = +prompt("Type number")
+//     var result = first%second
+//     console.log(result)
+// }
+// modolo()
+
+// function number(){
+//     var num = +prompt("Type number")
+//     num++
+//     console.log(num)
+// }
+// number()
+
+
+// function rad(){
+//     var radius = 1
+//     var scope = 2*3.14*radius
+//     var area = (3.14**radius)
+//     console.log(area,scope)
+// }
+// rad()
+
+
+
+
+
+
